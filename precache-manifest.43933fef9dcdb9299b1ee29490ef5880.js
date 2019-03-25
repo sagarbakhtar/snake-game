e724cc08c187f6d3941b7afda02f13580a346ed1
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-25 16:42:57 +0530

    Updates

---
+++ precache-manifest.43933fef9dcdb9299b1ee29490ef5880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  },
+  {
+    "revision": "be9c4302d2965adea603",
+    "url": "/snake-game/static/js/main.2cb116a8.chunk.js"
+  },
+  {
+    "revision": "be553234399cb8fcd546",
+    "url": "/snake-game/static/js/2.c3f70ae9.chunk.js"
+  },
+  {
+    "revision": "be9c4302d2965adea603",
+    "url": "/snake-game/static/css/main.27a58617.chunk.css"
+  },
+  {
+    "revision": "baded0ea94c697a2c2153bf7a53a88be",
+    "url": "/snake-game/index.html"
+  }
+];
